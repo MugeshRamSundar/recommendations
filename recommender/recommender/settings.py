165760934b985addb11f802043a3ea0082b68df0
Mugesh Ram Sundar G S <mugeshramsundar2004@gmail.com>
2024-09-06 15:22:39 +0530

    Update settings.py

--- recommender/recommender/settings.py
+++ recommender/recommender/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-(hz9(a0$93(9w5@=o50_&l1fa6j*m2hh8okh&y0i10ws7&cr$+
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
-    '9d46-14-102-13-146.ngrok-free.app'
+    '*'
 ]
 
 
@@ -77,12 +76,12 @@ WSGI_APPLICATION = 'recommender.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation

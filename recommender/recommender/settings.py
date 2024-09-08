8242307dee4dc8f051e4c5bb9354d4aeab02c386
Mugesh Ram Sundar G S <mugeshramsundar2004@gmail.com>
2024-09-08 12:48:09 +0530

    Update settings.py

--- recommender/recommender/settings.py
+++ recommender/recommender/settings.py
@@ -73,8 +73,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'recommender.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
